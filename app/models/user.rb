97030b45512b550b3d9e14a590d08850fed470da
RichardChileya <richardchileyawk@gmail.com>
2023-01-20 22:22:18 +0200

    Fix Linters errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
-  
+
   def most_recent_post
     posts.order(created_at: :desc).limit(3)
   end
