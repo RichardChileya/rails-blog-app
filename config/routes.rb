ed0880f54c609a28518e3a6903c1229cbaac0abf
RichardChileya <richardchileyawk@gmail.com>
2023-01-25 09:38:40 +0200

    add comfigured routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "users#index"
+    get "users/:id" => "users#show"
+
+    get "users/:user_posts" => "posts#index"
+    get "users/:user_post/:id" => "posts#show"
 end
