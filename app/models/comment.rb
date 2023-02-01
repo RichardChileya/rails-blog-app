96ea2ef9317db997bafb681ec4e1d85bd08100d4
RichardChileya <richardchileyawk@gmail.com>
2023-02-01 20:56:29 +0200

    Add controllers model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :post, class_name: 'Post'
   before_save :comments_counter
 
+  validates :text, presence: true
   def comments_counter
     post.update(comments_counter: post.comments.all.length)
   end
