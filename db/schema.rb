54eb8fecd7d8e692e95737ffa29fff45ca851f16
RichardChileya <richardchileyawk@gmail.com>
2023-02-09 21:05:43 +0200

    add role to users migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_09_091956) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_09_175126) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
